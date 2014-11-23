8525546979414d9b5174f262747925b3825a4b49
albertorivera <albertomarcosrivera@gmail.com>
2014-11-23 11:37:12 -0500

    Submitting my code for the second programming assingment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix allows me to change the matrix of interest, retrieve said matrix, 
+##assign an inverse, and retrieve said inverse 
 
-## Write a short comment describing this function
+## This was inspired by the example function, but I simply replaced mean with inverse,
+##abbreviated "inv". A stands for the inverse matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  A<-NULL
+  set<-function(z){
+    x<<-z
+    A<<-NULL
+  }
+  get<-function() x
+  setinv<-function(inv) A<<-inv
+  getinv<-function() A
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
+  
 }
 
 
-## Write a short comment describing this function
+
+## This is similarly based on the cachemean function from the example. Here C represents
+## the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  C<-x$getinv()
+  if(!is.null(C)){
+    message("getting cached data")
+    return(C)
+  }
+  data<-x$get()
+  C<-solve(data,...)
+  x$setinv(C)
+  C 
 }
+        ## Return a matrix that is the inverse of 'x'
+
